fab764483d9873228782b8273bae0fc9fd987ebc
Lauren Schroeder <schroederle@schroederle-mp.local>
2018-10-12 12:15:09 -0500

    http updates

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,56 @@ module.exports = function (app, addon) {
         }
     );
 
-    // Add any additional route handlers you need for views or REST resources here...
+    // Render the background-color macro.
+// Render the background-color macro.
+app.get('/v1/backgroundColor', function(req, res){
+
+    //  Grab all input parameters - sent through to us as query params.
+    var color       = req.query['color'],
+        pageId      = req.query['pageId'],
+        pageVersion = req.query['pageVersion'],
+        macroHash   = req.query['macroHash'],
+        userKey     = req.query['user_key'];
+    var clientKey = req.context.clientKey;
+
+    // Returns a HTTP client which can make calls to our host product.
+// @param clientKey formed when app created.
+// @param userKey formed when app created.
+// @returns {*} http client
+
+function getHTTPClient (clientKey, userKey){
+    return addon.httpClient({
+        clientKey : clientKey,
+        userKey   : userKey,
+        appKey    : addon.key
+    });
+}
+
+
+    //  Execute API request to get the macro body.
+    getHTTPClient(clientKey, userKey).get(
+        '/rest/api/content/' + pageId +
+        '/history/' + pageVersion +
+        '/macro/hash/' + macroHash,
+        function(err, response, contents){
+
+            //  If we've encountered errors, render an error screen.
+            if(err || (response.statusCode < 200 || response.statusCode > 299)) {
+                console.log(err);
+                res.render('<strong>An error has occurred :( '+ response.statusCode +'</strong>');
+            }
+
+            //  Parse the response, and send the body through.
+            contents = JSON.parse(contents);
+
+            //  Render with required body.
+            res.render('background-color', { body : contents.body, color: color });
+
+        }
+    );
+
+});
+ // Add any additional route handlers you need for views or REST resources here...
 
 
     // load any additional files you have in routes and apply those to the app
